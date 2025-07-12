97e93b44c929efd2cccba9e5a6de2403c450246f
Sérgio Ricardo <working7.sergio@gmail.com>
2025-07-12 17:01:19 -0300

    Update

--- Atividades/1.calculadora.py
+++ Atividades/1.calculadora.py
@@ -1,7 +1,7 @@
 print("***********************Bem-vindo à calculadora criada por Sérgio Ricardo com o auxílio da DSA****************************")
 num1 = float(input("Digite o primeiro número: "))
 num2 = float(input("Digite o segundo número: "))
-operação = input("Selecione um simbolo de uma operação: (soma(+), subtração(-), divisão(/), multiplicação(*), potenciação(**): ")
+operação = input("Selecione um simbolo de uma operação: (soma(+), subtração(-), divisão(/), multiplicação(*): ")
 if operação == "+":
     resultado = num1 + num2
     print("O resultado da soma é: ", resultado)
