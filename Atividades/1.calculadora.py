a51523d3f6b0578cd7c2cc1a1136dbcba9459191
Sérgio Ricardo <working7.sergio@gmail.com>
2025-07-12 11:53:10 -0300

    calculadora

---
+++ Atividades/1.calculadora.py
@@ -0,0 +1,21 @@
+print("***********************Bem-vindo à calculadora criada por Sérgio Ricardo com o auxílio da DSA****************************")
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+operação = input("Selecione um simbolo de uma operação: (soma(+), subtração(-), divisão(/), multiplicação(*), exponenciação(**): ")
+if operação == "+":
+    resultado = num1 + num2
+    print("O resultado é: ", resultado)
+elif operação == "-":
+    resultado = num1 - num2 
+    print("resultado é:", resultado)
+elif operação == "/":
+    resultado = num1/num2
+    print("O resultado é:", resultado)
+elif operação == "*":
+    resultado = num1*num2
+    print("O resultado é:", resultado)
+elif operação == "**":
+    resultado = num1**num2
+    print("O resultado é:", resultado)
+else:
+    print("Erro, pois não foi indentificado nenhuma das operações")
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# DataScienceAcademy_DSA
-Atividades, desafios e projetos
+# Ei, e aí. Tudo bem?!
+# Repositório em construção
+Essa simples atividade foi proposta pelo curso gratuito DataScienceAcademy(DSA). Por isso, o objetivo é praticar habilidades em python. A calculadora possui as operações de soma(+), subtração(-), divisão(/), multiplicação(*) e exponenciação(**). A princípio, segui o pseudocódigo para, assim, de acordo com a explicação do instrutor do curso, auxiliar nos passos para a criação dela. 
