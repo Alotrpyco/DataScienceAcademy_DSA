44db309a214a4129157800ae0066b0c9679371ff
Sérgio Ricardo <working7.sergio@gmail.com>
2025-07-12 13:22:31 -0300

    Update

--- Atividades/1.calculadora.py
+++ Atividades/1.calculadora.py
@@ -1,21 +1,19 @@
 print("***********************Bem-vindo à calculadora criada por Sérgio Ricardo com o auxílio da DSA****************************")
 num1 = float(input("Digite o primeiro número:"))
 num2 = float(input("Digite o segundo número: "))
-operação = input("Selecione um simbolo de uma operação: (soma(+), subtração(-), divisão(/), multiplicação(*), exponenciação(**): ")
+operação = input("Selecione um simbolo de uma operação: (soma(+), subtração(-), divisão(/), multiplicação(*), potenciação(**): ")
 if operação == "+":
     resultado = num1 + num2
-    print("O resultado é: ", resultado)
+    print("O resultado da soma é: ", resultado)
 elif operação == "-":
     resultado = num1 - num2 
-    print("resultado é:", resultado)
+    print("resultado da subtração é:", resultado)
 elif operação == "/":
     resultado = num1/num2
-    print("O resultado é:", resultado)
+    print("O resultado divisão é:", resultado)
 elif operação == "*":
     resultado = num1*num2
-    print("O resultado é:", resultado)
-elif operação == "**":
-    resultado = num1**num2
-    print("O resultado é:", resultado)
+    print("O resultado da multiplicação é:", resultado)
+
 else:
     print("Erro, pois não foi indentificado nenhuma das operações")
